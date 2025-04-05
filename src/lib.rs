7da204a717dcf5099b37be039887e4fe3fc54f51
axel <sagit117@gmail.com>
2025-04-05 10:12:53 +0300

    dock

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,36 @@ impl Drop for ThreadPool {
     }
 }
 
+/// Пул потоков.
+/// 
+/// #Example
+/// 
+/// ```
+/// use std::{thread, time::Duration};
+/// use speck_thread_pool::ThreadPool;
+/// let pool = ThreadPool::new(4).unwrap();
+/// pool.execute(|| {
+///     thread::sleep(Duration::new(1, 0));
+/// println!("execute");
+/// });
+/// pool.execute(|| {
+///     thread::sleep(Duration::new(2, 0));
+///     println!("execute 0");   
+/// });
+/// pool.execute(|| {
+///     thread::sleep(Duration::new(2, 0));
+///     println!("execute 1");    
+/// });
+/// pool.execute(|| {
+///     thread::sleep(Duration::new(2, 0));
+///     println!("execute 2");
+/// });
+/// pool.execute(|| {
+///     println!("execute last");
+/// });
+/// thread::sleep(Duration::new(3, 0));
+///```
+
 impl ThreadPool {
     /// Создайте новый ThreadPool.
     ///
